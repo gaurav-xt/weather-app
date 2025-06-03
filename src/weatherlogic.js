36a6b6a4d323bbef5da57aa28a3416f726f4eb22
gaurav-xt <gauravtambare7@gmail.com>
2025-06-03 21:26:11 +0530

    HII

--- src/weatherlogic.js
+++ src/weatherlogic.js
@@ -1,5 +1,5 @@
 async function getCityInfo(){
-  const city = "nagpur";
+  const city =document.getElementById("search-loco").value;
   const apiKey = "8f530d72fd0c60c1c28120ad610c41be";  // Replace with your OpenWeatherMap API key
 
   const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
@@ -10,4 +10,9 @@ async function getCityInfo(){
  
 }
 getCityInfo()
+
+let city = document.getElementById("search-loco").value;
+
+console.log(city);
+
 
